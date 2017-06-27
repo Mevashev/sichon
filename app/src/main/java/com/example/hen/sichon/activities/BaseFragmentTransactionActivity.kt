d594638b72d210f5e0ba08465318b18b5203241b
Slava Voltfman <slava.voltfman@falcore.co>
2017-06-27 16:30:30 +0300

    merging phrases category fragment to app flow

--- app/src/main/java/com/example/hen/sichon/activities/BaseFragmentTransactionActivity.kt
+++ app/src/main/java/com/example/hen/sichon/activities/BaseFragmentTransactionActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hen.sichon
+package com.example.hen.sichon.activities
 
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.example.hen.sichon.R
 
 /**
  * Created by slava-android on 6/27/2017.
@@ -13,9 +14,9 @@ open class BaseFragmentTransactionActivity : AppCompatActivity() {
         val ft = supportFragmentManager.beginTransaction()
 
         if (addToBackStack) {
-            ft.add(R.id.fragments_container, fragment, fragment.javaClass.simpleName).addToBackStack(fragment.javaClass.simpleName).commit()
+            ft.replace(R.id.fragments_container, fragment, fragment.javaClass.simpleName).addToBackStack(fragment.javaClass.simpleName).commit()
         } else {
-            ft.add(R.id.fragments_container, fragment, fragment.javaClass.simpleName).commit()
+            ft.replace(R.id.fragments_container, fragment, fragment.javaClass.simpleName).commit()
         }
     }
 }
