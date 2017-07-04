111e470e88462698258873f7baf28634e487a65d
FD0077\Hen Mevashev <hen@falcore.co>
2017-07-04 18:27:40 +0300

    favorites

--- app/src/main/java/com/example/hen/sichon/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/example/hen/sichon/fragments/FavoritesFragment.kt
@@ -2,9 +2,12 @@ package com.example.hen.sichon.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.example.hen.sichon.R
 
 /**
@@ -20,5 +23,13 @@ class FavoritesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        val phrasesRecyclerView = view?.findViewById(R.id.recycler_view_phrases) as RecyclerView
+
+        phrasesRecyclerView.layoutManager = LinearLayoutManager(activity.baseContext, LinearLayout.VERTICAL, false)
     }
 }
